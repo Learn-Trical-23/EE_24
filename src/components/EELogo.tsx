a2aeabb4ba64af717a5dba1adc869aa2b21dca39
piriyatharsanm94-lang <piriyatharsanm94@gmail.com>
2026-02-07 05:48:52 +0530

    new

--- src/components/EELogo.tsx
+++ src/components/EELogo.tsx
@@ -2,7 +2,7 @@
 import React from "react";
 
 const EELogo = () => (
-  <img src="/ee-logo.jpg" alt="EE Logo" className="h-10 w-10 mr-2" style={{ display: "inline-block" }} />
+  <img src="/EE_24/ee-logo.jpg" alt="EE Logo" className="h-10 w-10 mr-2" style={{ display: "inline-block" }} />
 );
 
 export default EELogo;
